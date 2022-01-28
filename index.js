c2ec316c30cff64c3019c56914be1300fd4a7f18
PedroSehn <pedrorsehn@hotmail.com>
2022-01-28 15:16:56 -0300

    alinhamento de linter

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const productController = require('./controllers/productController');
-const port = process.env.PORT || 3000;
 
+const app = express();
 const joi = require('joi');
+const productController = require('./controllers/productController');
+
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -16,32 +17,31 @@ app.get('/', (_request, response) => {
 
 app.use('/products', productController);
 
-
 app.get('/products', (req, res) => {
   const result = productController.getAll();
   res.status(200).json(result);
 });
 
-//midlewere de erro JOI
+// midlewere de erro JOI
 app.use((err, req, res, next) => {
-    if(!joi.isError(err)) {
+    if (!joi.isError(err)) {
       return next(err);
     }
 
     res
       .status(422)
-      .json( { code: 'unprocessable_entity', message: err.message });
+      .json({ code: 'unprocessable_entity', message: err.message });
 });
 
-//middleware erro dominio
+// middleware erro dominio
 app.use((err, req, res, next) => {
   const errorMap = {
-    not_found: 404
-  }
+    notFound: 404,
+  };
 
-  const status = errorMap[err.code]
+  const status = errorMap[err.code];
 
-  if(!status) {
+  if (!status) {
     return next(err);
   }
 
@@ -49,10 +49,10 @@ app.use((err, req, res, next) => {
   .json(err);
 });
 
-//middlewere erros gerais
-app.use((err, req, res, next) => {
+// middlewere erros gerais
+app.use((err, req, res, _next) => {
   console.error(err);
-  res.status(500).json({ code: 'internal_server_error', message: 'error processing request'})
+  res.status(500).json({ code: 'internal_server_error', message: 'error processing request' });
 });
 
 app.listen(port, () => {
